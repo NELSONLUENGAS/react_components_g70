346c918484ea3f9f0564b5a3fd2a05c27d6bc6a8
NELSON ANDRES LUENGAS SILVA <90053867+NELSONLUENGAS@users.noreply.github.com>
2024-08-09 08:56:35 -0500

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://nelsonluengas.github.io/react_components_g70"
+  base: "https://NELSONLUENGAS.github.io/react_components_g70"
 })
